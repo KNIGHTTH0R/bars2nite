98a23e99e706c185fd24fac59c685db44d909e4e
Catherine Lin <catherine.lin.93@gmail.com>
2019-03-05 23:01:06 -0800

    added more testing to AllEvents

--- client/src/components/__test__/AllEvents.test.js
+++ client/src/components/__test__/AllEvents.test.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Root from '../../Root';
-import AllEvents from '../AllEvents';
+import { AllEvents } from '../AllEvents';
 
 describe('AllEvents', () => {
-  const allEvents = shallow(
-    <Root>
-      <AllEvents />
-    </Root>
-  );
+  const mockOnLoadUserReservedBars = jest.fn();
+  const mockOnLoadReservedBars = jest.fn();
+
+  const props = {
+    onLoadUserReservedBars: mockOnLoadUserReservedBars,
+    onLoadReservedBars: mockOnLoadReservedBars
+  };
+
+  let allEvents = shallow(<AllEvents {...props} />);
   it('renders properly', () => {
     expect(allEvents).toMatchSnapshot();
   });
 
-  it('renders the page title', () => {
-    expect(allEvents.find('.Event__heading').text()).toEqual('All Events');
+  describe('when mounted', () => {
+    beforeEach(() => {
+      allEvents = mount(<AllEvents {...props} />);
+    });
+
+    it('renders the page title', () => {
+      expect(allEvents.find('.Event__heading').text()).toEqual('All Events');
+    });
+
+    it('dispatches the `onLoadUserReservedBars` and `onLoadReservedBars` it receives from props', () => {
+      expect(mockOnLoadUserReservedBars).toHaveBeenCalled();
+      expect(mockOnLoadReservedBars).toHaveBeenCalled();
+    });
   });
 });
